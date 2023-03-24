9541427f2e653520f085565c038bcd6ca8bd85fe
Luanda Dantas <ludanttas@gmail.com>
2023-03-23 21:58:30 -0300

    Save content from vultr website in csv file

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from pprint import pprint
 
 from scrape.vultr import vultr_scrape
-from utils import save_json
+from utils import save_json, save_csv
 
 
 if __name__ == "__main__":
-    scrap_content = vultr_scrape()
+    scrape_content = vultr_scrape()
 
-    save_json(scrap_content)
-    pprint(scrap_content)
+    save_json(scrape_content)
+    save_csv(scrape_content)
+    pprint(scrape_content)
--- utils.py
+++ utils.py
@@ -1,8 +1,19 @@
 import json
+import csv
 
 
-def save_json(scrap_content):
+def save_json(scrape_content):
     with open("scrape_content.json", "w") as write_file:
-        json.dump(scrap_content, write_file, indent=4)
+        json.dump(scrape_content, write_file, indent=4)
+
+    write_file.close()
+
+
+def save_csv(scrape_content):
+    fieldnames = ['price', 'storage', 'bandwidth', 'cpu', 'memory']
+    with open("scrape_content.csv", "w") as write_file:
+        writer = csv.DictWriter(write_file, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(scrape_content)
 
     write_file.close()
